175ec87cf394113a2e7a5003fecd5fc11cca133f
Kiran Hegde <kiransrivani@gmail.com>
2025-10-23 13:09:07 -0500

    fnn code

--- ffnn.py
+++ ffnn.py
@@ -80,25 +80,21 @@ def convert_to_vector_representation(data, word2index):
 
 
 
-def load_data(train_data, val_data, test_data):
+def load_data(train_data, val_data):
     with open(train_data) as training_f:
         training = json.load(training_f)
     with open(val_data) as valid_f:
         validation = json.load(valid_f)
-    with open(test_data) as test_data_f:
-        test = json.load(test_data_f)
+
 
     tra = []
     val = []
-    tes = []
     for elt in training:
         tra.append((elt["text"].split(),int(elt["stars"]-1)))
     for elt in validation:
         val.append((elt["text"].split(),int(elt["stars"]-1)))
-    for elt in test:
-        tes.append((elt["text"].split(),int(elt["stars"]-1)))
 
-    return tra, val, tes
+    return tra, val
 
 
 if __name__ == "__main__":
@@ -117,14 +113,13 @@ if __name__ == "__main__":
 
     # load data
     print("========== Loading data ==========")
-    train_data, valid_data, test_data = load_data(args.train_data, args.val_data, args.test_data) # X_data is a list of pairs (document, y); y in {0,1,2,3,4}
+    train_data, valid_data = load_data(args.train_data, args.val_data) # X_data is a list of pairs (document, y); y in {0,1,2,3,4}
     vocab = make_vocab(train_data)
     vocab, word2index, index2word = make_indices(vocab)
 
     print("========== Vectorizing data ==========")
     train_data = convert_to_vector_representation(train_data, word2index)
     valid_data = convert_to_vector_representation(valid_data, word2index)
-    test_data = convert_to_vector_representation(test_data, word2index)
 
     model = FFNN(input_dim = len(vocab), h = args.hidden_dim)
     optimizer = optim.SGD(model.parameters(),lr=0.01, momentum=0.9)
@@ -140,11 +135,15 @@ if __name__ == "__main__":
         random.shuffle(train_data) # Good practice to shuffle order of training data
         minibatch_size = 16
         N = len(train_data)
-        for minibatch_index in tqdm(range(N // minibatch_size)):
+        for minibatch_index in tqdm(range(0, N, minibatch_size)):
             optimizer.zero_grad()
             loss = None
-            for example_index in range(minibatch_size):
-                input_vector, gold_label = train_data[minibatch_index * minibatch_size + example_index]
+
+            actual_batch = train_data[minibatch_index: minibatch_index + minibatch_size]
+            actual_batch_size = len(actual_batch)
+
+            for example_index in range(actual_batch_size):
+                input_vector, gold_label = actual_batch[example_index]
                 predicted_vector = model(input_vector)
                 predicted_label = torch.argmax(predicted_vector)
                 correct += int(predicted_label == gold_label)
@@ -154,7 +153,7 @@ if __name__ == "__main__":
                     loss = example_loss
                 else:
                     loss += example_loss
-            loss = loss / minibatch_size
+            loss = loss / actual_batch_size
             loss.backward()
             optimizer.step()
         print("Training completed for epoch {}".format(epoch + 1))
@@ -170,10 +169,14 @@ if __name__ == "__main__":
         minibatch_size = 16
         N = len(valid_data)
         with torch.no_grad():
-            for minibatch_index in tqdm(range(N // minibatch_size)):
+            for minibatch_index in tqdm(range(0, N, minibatch_size)):
                 loss = None
-                for example_index in range(minibatch_size):
-                    input_vector, gold_label = valid_data[minibatch_index * minibatch_size + example_index]
+
+                actual_batch = valid_data[minibatch_index: minibatch_index + minibatch_size]
+                actual_batch_size = len(actual_batch)
+
+                for example_index in range(actual_batch_size):
+                    input_vector, gold_label = actual_batch[example_index]
                     predicted_vector = model(input_vector)
                     predicted_label = torch.argmax(predicted_vector)
                     correct += int(predicted_label == gold_label)
@@ -183,25 +186,7 @@ if __name__ == "__main__":
                         loss = example_loss
                     else:
                         loss += example_loss
-                loss = loss / minibatch_size
+                loss = loss / actual_batch_size
         print("Validation completed for epoch {}".format(epoch + 1))
         print("Validation accuracy for epoch {}: {}".format(epoch + 1, correct / total))
         print("Validation time for this epoch: {}".format(time.time() - start_time))
-
-        model.eval()
-        correct = 0
-        total = 0
-        start_time = time.time()
-        minibatch_size = 16
-        N = len(test_data)
-        with torch.no_grad():
-            for minibatch_index in tqdm(range(N // minibatch_size)):
-                for example_index in range(minibatch_size):
-                    input_vector, gold_label = test_data[minibatch_index * minibatch_size + example_index]
-                    predicted_vector = model(input_vector)
-                    predicted_label = torch.argmax(predicted_vector)
-                    correct += int(predicted_label == gold_label)
-                    total += 1
-
-        print("Test accuracy: {}".format(correct / total))
-        print("Test time: {}".format(time.time() - start_time))
